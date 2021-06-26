b17dfdae6bd816b2f52565804b472b12a9468c59
Md. Jehadul Karim <sabit.jehadul.karim@gmail.com>
2021-06-27 02:57:19 +0600

    Messenger4Java Added

--- backend/src/main/java/com/memoryleak/bloodbank/BackendApplication.java
+++ backend/src/main/java/com/memoryleak/bloodbank/BackendApplication.java
@@ -1,10 +1,20 @@
 package com.memoryleak.bloodbank;
 
+import com.github.messenger4j.Messenger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
+
+    @Bean
+    public Messenger messenger(@Value("${messenger4j.pageAccessToken}") String pageAccessToken,
+                               @Value("${messenger4j.appSecret}") final String appSecret,
+                               @Value("${messenger4j.verifyToken}") final String verifyToken) {
+        return Messenger.create(pageAccessToken, appSecret, verifyToken);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
